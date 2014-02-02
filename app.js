024c2218b5a472fd431602eb6e38ec4a201bd0b0
Benjamin Landers <benjaminrlanders@gmail.com>
2014-02-02 13:09:54 -0800

    added search

--- app.js
+++ app.js
@@ -9,29 +9,36 @@ app.use(express.static(__dirname+'/static'));
 app.use('/uploads',express.static(__dirname+'/uploads'));
 app.use(express.bodyParser({uploadDir:'./uploads'}));
 app.get('/get', function(req, res){
-  db.items.find(req.query,function(err,items){
+  get(req.query,res);
+});
+app.post('/get', function(req, res){
+  get(req.body,res);
+});
+function get(body,res){
+  db.items.find(body,function(err,items){
     res.send(items);
   }).count(function(err,res){
-    console.log("Requested " + JSON.stringify(req.query) + "; " + res + " items found.");
+    console.log("Requested " + JSON.stringify(body) + "; " + res + " items found.");
   });
-});
+}
 app.get('/create', function(req, res){
   console.log(req.query);
-  db.items.insert(req.query,function(err,data){
-    res.send(data);
-  });
+  create(req.query,res);
 });
 app.post('/create',function(req,res){
   console.log(req.body);
   var ext = path.extname(req.files.file.name).toLowerCase();
   if(ext === '.png' || ext === '.jpeg' || ext === '.jpg' || ext === '.gif'){
     req.body.image = req.files.file.path;
-    db.items.insert(req.body,function(err,data){
-      res.send(data);
-    });
+    create(req.body,res);
   }else{
-    throw 'File type Error'
+    throw 'File type Error';
   }
 
 });
+function create(body,res){
+  db.items.insert(body,function(err,data){
+    res.send(data);
+  });
+}
 app.listen(3000);
